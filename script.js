eba3875999c78e4b36413de1c78386c03355d273
Vagner ferreira <euvagnerferreira@gmail.com>
2024-03-27 21:59:35 -0300

    Atualizações novas

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
-function divideGrupos() {
+function dividesGroups() {
   const namesInput = document.getElementById('names').value.trim();
   const namesArray = namesInput.split('\n').map(name => name.trim());
 
-  const numGrupos = parseInt(document.getElementById('num-groups').value);
-  if (numGrupos < 1) {
-    alert('A quantidade de grupos deve ser no mínimo 1.');
+ 
+
+  const numGroups = parseInt(document.getElementById('num-groups').value);
+  if (numGroups < 1) {
+    alert('O número de grupos deve ser pelo menos 1.');
     return;
   }
 
   const shuffledNames = shuffleArray(namesArray);
-  const grupos = [];
 
-  for (let i = 0; i < numGrupos; i++) {
-    grupos.push([]);
+  const groups = [];
+  for (let i = 0; i < numGroups; i++) {
+    groups.push([]);
   }
 
-  let grupoIndex = 0;
+  let groupIndex = 0;
   shuffledNames.forEach(name => {
-    grupos[grupoIndex].push(name);
-    grupoIndex = (grupoIndex + 1) % numGrupos;
+    groups[groupIndex].push(name);
+    groupIndex = (groupIndex + 1) % numGroups;
   });
 
   const outputDiv = document.getElementById('output');
   outputDiv.innerHTML = '';
-
-  grupos.forEach((grupo, index) => {
-    outputDiv.innerHTML += `<p><strong>Grupo ${index + 1}:</strong> ${grupo.join(', ')}</p>`;
+  groups.forEach((group, index) => {
+    outputDiv.innerHTML += `<p><strong>Grupo ${index + 1}:</strong> ${group.join(', ')}</p>`;
   });
 }
+
 
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
   }
   return array;
 }
